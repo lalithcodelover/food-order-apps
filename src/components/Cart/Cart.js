9fa34f831cb705ae727b7080eef19162767de73a
lalithcodelover <lalith.vlr@gmail.com>
2022-11-14 17:30:56 +0530

    Cart with add remove functionality added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,26 +1,58 @@
-import Modal from '../UI/Modal'
-import classes from './Cart.module.css'
-import CartContext from '../../store/cart-context'
-import { useContext } from 'react'
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
+import { useContext } from "react";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
 
-const cartCtx = useContext(CartContext)
-const cartItems= <ul>{cartCtx.items.map((item)=>{return <li key={item.id}>{item.name}</li>})}</ul>
+  const increaseQuantityHandler= (item) =>{
+     cartCtx.addItem({...item,quantity:1})
+  }
+  const decreaseQuantityHandler= (id)=>{
+    cartCtx.removeItem(id)
+  }
+  const cartItems = (
+    <ul className={classes['cart-items']}>
+      {cartCtx.items.map((item) => {
 
-return (
-        <Modal onClose={props.onClose}>
-            {cartItems}
-            <div className={classes.total}>
-                <span>Total Amount</span>
-                <span>$25.55</span>
+
+        return <li key={item.id}>
+            <div>
+            <h3>{item.name}</h3>
+            <span>{item.quantity}</span>
+            <div className={classes.price}>${item.price.toFixed(2)}</div>
             </div>
-            <div className={classes.actions}>
-                <button onClick={props.onClose} className={classes['button--alt']}>Close</button>
-                <button className={classes.button}>Order</button>
+            
+            <div className={classes.alter}>
+<button className={classes.decrease} onClick={()=>decreaseQuantityHandler(item.id)}>-</button>
+<button className={classes.increase} onClick={()=>increaseQuantityHandler(item)}>+</button>
             </div>
-            </Modal>
-    )
-}
+            </li>;
+      })}
+    </ul>
+  );
 
-export default Cart
+  let amount = 0;
+  cartCtx.items.forEach((item) => {
+    amount = amount + Number(item.price * item.quantity);
+  });
+
+  return (
+    <Modal onClose={props.onClose}>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>${amount.toFixed(2)}</span>
+      </div>
+      <div className={classes.actions}>
+        <button onClick={props.onClose} className={classes["button--alt"]}>
+          Close
+        </button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -6,12 +6,49 @@ const CartProvider = (props) => {
     const [addItems,setAddItems] = useState([])
     
     const addItemToCartHandler=(item)=>{
-        setAddItems((prevItems)=>{
-            return [...prevItems,item]
+        let hasItem = false;
+        const newArray =[...addItems]
+        newArray.forEach((product,index)=>{
+            if(product.id===item.id){
+                hasItem=true;
+                newArray[index].quantity=Number(newArray[index].quantity)+Number(item.quantity)
+            }
         })
+        if(hasItem){
+            setAddItems(newArray)
+        }
+        else{
+            setAddItems((prevItems)=>{
+                       
+                return [...prevItems,item]
+            })
+        }
     }
 
-    const removeItemFromCartHandler=(id)=>{}
+    const removeItemFromCartHandler=(id)=>{
+        let hasItem = false;
+        const newArray =[...addItems]
+        newArray.forEach((product,index)=>{
+            if(product.id===id && product.quantity===1){
+                hasItem=true;
+              const temp=  newArray.splice(index,1)
+              setAddItems(temp)
+            }
+            else if(product.id===id){
+                hasItem=true;
+                newArray[index].quantity=Number(newArray[index].quantity)-1
+            }
+        })
+        if(hasItem){
+            setAddItems(newArray)
+        }
+        else{
+            setAddItems((prevItems)=>{
+                       
+                return [...prevItems]
+            })
+        }
+    }
 
 
 const cartContext={
